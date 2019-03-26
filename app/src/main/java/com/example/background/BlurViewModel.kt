571217bd7229b8be95e2ff9c9f29a93dcb3dbcc3
Chris Rodriguez <cerdevelopers@gmail.com>
2019-03-25 20:14:44 -0500

    UPDATE: • Now blurs one photo at a time

--- app/src/main/java/com/example/background/BlurViewModel.kt
+++ app/src/main/java/com/example/background/BlurViewModel.kt
@@ -18,10 +18,7 @@ package com.example.background
 
 import android.net.Uri
 import androidx.lifecycle.ViewModel
-import androidx.work.Data
-import androidx.work.OneTimeWorkRequest
-import androidx.work.OneTimeWorkRequestBuilder
-import androidx.work.WorkManager
+import androidx.work.*
 import com.example.background.workers.BlurWorker
 import com.example.background.workers.CleanupWorker
 import com.example.background.workers.SaveImageToFileWorker
@@ -58,8 +55,11 @@ class BlurViewModel : ViewModel() {
     internal fun applyBlur(blurLevel: Int) {
         // Add WorkRequest to Cleanup temporary images
         var continuation = workManager
-                .beginWith(OneTimeWorkRequest
-                        .from(CleanupWorker::class.java))
+                .beginUniqueWork(
+                        IMAGE_MANIPULATION_WORK_NAME,
+                        ExistingWorkPolicy.REPLACE,
+                        OneTimeWorkRequest.from(CleanupWorker::class.java)
+                )
 
         // Add WorkRequests to blur the image the number of times requested
         for (i in 0 until blurLevel) {
